516e26ef6d86da00500a8ff5a3b2d1b3ed1f385d
Zhongpeng Lin <lin.zhp@gmail.com>
2012-04-25 15:48:09 -0700

    commit missing file from last commit

---
+++ js/FreeShapeHandler.js
@@ -0,0 +1,23 @@
+function FreeShapeHandler(draw) {
+	this.draw = draw;
+}
+
+FreeShapeHandler.prototype.onMouseDown = function(x, y){
+	this.selected = this.draw.path('M '+x+' '+y);
+};
+
+FreeShapeHandler.prototype.onMouseMove = function(x, y){
+	if(this.selected){
+		var path = this.selected.attrs.path;
+		this.selected.attr('path', path +' L ' + x + ' ' + y);
+	}
+};
+
+FreeShapeHandler.prototype.onMouseUp = function(x, y){
+	if(this.selected){
+		var path = this.selected.attrs.path;
+		this.selected.attr('path', path +' Z');
+		this.current = this.selected;
+		this.selected = null;
+	}
+};
