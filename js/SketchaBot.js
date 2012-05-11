70306d09f80d5469feedd81235710a234ddd62d4
Huascar A. Sanchez <hsanchez@soe.ucsc.edu>
2012-05-11 16:01:05 -0700

    refactored class so that it matches a coding standard for defining classes in Javascript.

--- js/SketchaBot.js
+++ js/SketchaBot.js
@@ -2,210 +2,221 @@
  * SketchaBot functionality
  * @author hsanchez@cs.ucsc.edu (Huascar A. Sanchez)
  */
-var BOTPRINT = BOTPRINT || {};
-
-BOTPRINT.SketchaBot = new function() {
-	// Use self to reduce confusion about 'this'
-	var self 	 = this;
-
-	// internal vars
-	var canvas  	= new Canvas2D('canvas2d');
-	var preview 	= new Preview3D('preview3d');
-	var running 	= true;
-	var previewing 	= true;
-
-	var vars	= []; // this will contain the choices we have made in the side bar
-
-	var $container 	= $('#container');
-
-	var width			= $container.width(),
-		height			= $container.height();
-
-	var $gui		= $('#gui');
-
-	// predefined shape to be previewed by the SketchaBot app
-	var geometry = new THREE.CubeGeometry( 100, 100, 100 );
-	var material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
-	var mesh 	 = new THREE.Mesh( geometry, material );
-
-	preview.setObject(mesh);
-
+(function(Raphael){
 
 	/**
-	 * Pauses the 3D preview animation
+	 * factory method for creating a new sketchabot object
 	 */
-	self.pause = function() {
-		running = false;
+	Raphael.sketchabot = function() {
+		return new SketchaBot();
 	};
 
-	/**
-	 * Plays the 3D preview animation
-	 */
-	self.play = function() {
-		// init whatever we need, man!
-		self.init();
-		if(!running) {
-			running = true;
-			preview.animate();
-		} else {
-			preview.animate();
-		}
-
-		setInterval(function(){
-			//  Previews the 3d shape as long as it is indicated.
-			// 	This interval will give the browser chance to
-			//  extrude the shape object
-			if(previewing){
-				self.preview();
-			}
-		}, 100);
-	};
+	// Current version.
+	Raphael.sketchabot.VERSION = '0.0.1';
 
 	/**
-	 * previews the sketch in 3D.
+	 * The SketchaBot object.
 	 */
-	self.preview = function(){
-		var svg = canvas.getCurrentShape();
-		if(svg){
-			var toShape = new SVG2Shape();
-			var shape 	= toShape.convert(svg);
-			var mesh 	= new Chassis(shape, 50);
-			mesh.rotation.x = Math.PI/2;
-			preview.setObject(mesh);
-		}
-	};
+	var SketchaBot = function() {
+		// Use self to reduce confusion about 'this'
+		var self 	 = this;
 
-	/**
-	 * Initializes the SketchaBot experiment and kicks everything off. Yay!
-	 */
-	self.init = function() {
-		// set up our initial vars
-		vars["shape"]				= "Free";
-		vars["color"]				= "#00FF00";
-		vars["wheelsLocation"]		= false;
-		vars["show3dPreview"]		= true;
+		// internal vars
+		var canvas  	= new Canvas2D('canvas2d');
+		var preview 	= new Preview3D('preview3d');
+		var running 	= true;
+		var previewing 	= true;
 
-		// add listeners
-		addEventListeners();
+		var vars	= []; // this will contain the choices we have made in the side bar
 
-		// create our stuff
-		if(bootstrapCanvas2D()) {
-			setupUIComponents();
-			$gui.addClass('live');
-		} else {
-			$('html').removeClass('webgl').addClass('no-webgl');
-		}
-	};
+		var $container 	= $('#container');
 
-	function bootstrapCanvas2D() {
-		// TODO(Huascar) refactor this class around canvas2d
-		canvas.width  = width;
-		canvas.height = height;
-		return true;
-	}
+		var width			= $container.width(),
+			height			= $container.height();
 
-	function addEventListeners() {
-		// window event
-		$(window).resize(callbacks.windowResize);
+		var $gui		= $('#gui');
 
-		// GUI events
-		$(".gui-set a").click(callbacks.guiClick);
-		$(".gui-set a.default").trigger('click');
-	}
+		// predefined shape to be previewed by the SketchaBot app
+		var geometry = new THREE.CubeGeometry( 100, 100, 100 );
+		var material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
+		var mesh 	 = new THREE.Mesh( geometry, material );
 
-	/**
-	 * Creates the objects we need
-	 */
-	function setupUIComponents() {
-		// todo(Huascar) improve UI
-	}
-
-	/**
-	 * Simple handler function for the events we don't care about.
-	 * //todo(Huascar) use this method for DnD functionality....to code soon.
-	 */
-	function cancel(event) {
-		if(event.preventDefault) {
-			event.preventDefault();
-		}
-
-		return false;
-	}
+		preview.setObject(mesh);
 
 
-	function updateCanvasHandler (shape, color, wheels, preview){
-		var opts = {
-			stroke: "#F8F8F8 ",
-			"stroke-opacity": 1,
-			fill: color,
-			"stroke-width": 2,
-			"stroke-linecap": "round",
-			"stroke-linejoin": "round"
+		/**
+		 * Pauses the 3D preview animation
+		 */
+		self.pause = function() {
+			running = false;
 		};
 
-		previewing = preview;
-		switch(shape){
-			case "Free":
-				canvas.setOptions(opts);
-				canvas.setHandler(wheels && canvas.getCurrentShape() ? CircleHandler : FreeShapeHandler);
-				break;
-			case "Square":
-				canvas.setOptions(opts);
-				canvas.setHandler(wheels && canvas.getCurrentShape() ? CircleHandler : RectangleHandler);
-				break;
-			case "Polygon":
-				canvas.setOptions(opts);
-				canvas.setHandler(wheels && canvas.getCurrentShape() ? CircleHandler : PolygonHandler);
-				break;
-			default : alert("Unable to find selected shape.");
-		}
-	}
-
-	/**
-	 * Our internal callbacks object - a neat
-	 * and tidy way to organise the various
-	 * callbacks in operation.
-	 */
-	callbacks = {
-		windowResize: function() {
-			width			= $container.width();
-			height			= $container.height();
-		},
-
-		guiClick:function() {
-			var $this 	= $(this),
-				varName	= $this.data("guivar"),
-				varVal	= $this.data("guival");
-			if(vars[varName] !== null) {
-				if(varName == "wheelsLocation") {
-					vars[varName] = varVal;
-					if(vars[varName] && !canvas.getCurrentShape()){
-						vars[varName] = false;
-						alert("You must sketch chassis first!")
-						return false;
-					}
-				} else {
-					vars[varName] = varVal;
-				}
+		/**
+		 * Plays the 3D preview animation
+		 */
+		self.play = function() {
+			// init whatever we need, man!
+			self.init();
+			if(!running) {
+				running = true;
+				preview.animate();
+			} else {
+				preview.animate();
 			}
 
-			$this.siblings().addClass('disabled');
-			$this.removeClass('disabled');
+			setInterval(function(){
+				//  Previews the 3d shape as long as it is indicated.
+				// 	This interval will give the browser chance to
+				//  extrude the shape object
+				if(previewing){
+					self.preview();
+				}
+			}, 100);
+		};
 
-			// TODO(Huascar) refactor this....there is a better way to do this.
-			updateCanvasHandler(vars["shape"], vars["color"], vars["wheelsLocation"], vars["show3dPreview"]);
+		/**
+		 * Previews the sketch in 3D.
+		 */
+		self.preview = function(){
+			var svg = canvas.getCurrentShape();
+			if(svg){
+				var toShape = new SVG2Shape();
+				var shape 	= toShape.convert(svg);
+				var mesh 	= new Chassis(shape, 50);
+				mesh.rotation.x = Math.PI/2;
+				preview.setObject(mesh);
+			}
+		};
 
-			$this.used = true;
+		/**
+		 * Initializes the SketchaBot experiment and kicks everything off. Yay!
+		 */
+		self.init = function() {
+			// set up our initial vars
+			vars["shape"]				= "Free";
+			vars["color"]				= "#00FF00";
+			vars["wheelsLocation"]		= false;
+			vars["show3dPreview"]		= true;
+
+			// create our stuff
+			if(bootstrapCanvas2D()) {
+				setupUIComponents();
+				$gui.addClass('live');
+			} else {
+				$('html').removeClass('webgl').addClass('no-webgl');
+			}
+		};
+
+		/**
+		 * This function will be used to
+		 * @return {Boolean}
+		 */
+		function bootstrapCanvas2D() {
+			// add listeners
+			addEventListeners();
+
+			canvas.width  = width;
+			canvas.height = height;
+			return true;
+		}
+
+		function addEventListeners() {
+			// window event
+			$(window).resize(callbacks.windowResize);
+
+			// GUI events
+			$(".gui-set a").click(callbacks.guiClick);
+			$(".gui-set a.default").trigger('click');
+		}
+
+		/**
+		 * Creates the objects we need to make the UI nicer.
+		 */
+		function setupUIComponents() {
+			// todo(Huascar) improve UI
+		}
+
+		/**
+		 * Simple handler function for the events we don't care about.
+		 * //todo(Huascar) use this method for DnD functionality....to code soon.
+		 */
+		function cancel(event) {
+			if(event.preventDefault) {
+				event.preventDefault();
+			}
+
 			return false;
 		}
+
+
+		function updateCanvasHandler (shape, color, wheels, preview){
+			var opts = {
+				stroke: "#F8F8F8 ",
+				"stroke-opacity": 1,
+				fill: color,
+				"stroke-width": 2,
+				"stroke-linecap": "round",
+				"stroke-linejoin": "round"
+			};
+
+			previewing = preview;
+			var wheelsWithoutChassis = wheels && canvas.getCurrentShape();
+
+			switch(shape){
+				case "Free":
+					canvas.setOptions(opts);
+					canvas.setHandler(wheelsWithoutChassis ? CircleHandler : FreeShapeHandler);
+					break;
+				case "Square":
+					canvas.setOptions(opts);
+					canvas.setHandler(wheelsWithoutChassis ? CircleHandler : RectangleHandler);
+					break;
+				case "Polygon":
+					canvas.setOptions(opts);
+					canvas.setHandler(wheelsWithoutChassis ? CircleHandler : PolygonHandler);
+					break;
+				default : alert("Unable to find selected shape.");
+			}
+		}
+
+		/**
+		 * Our internal callbacks object - a neat
+		 * and tidy way to organise the various
+		 * callbacks in operation.
+		 */
+		callbacks = {
+			windowResize: function() {
+				width			= $container.width();
+				height			= $container.height();
+			},
+
+			guiClick:function() {
+				var $this 	= $(this),
+					varName	= $this.data("guivar");
+
+				vars[varName] = $this.data ("guival");
+
+				$this.siblings().addClass('disabled');
+				$this.removeClass('disabled');
+
+				// TODO(Huascar) refactor this....there is a better way to do this.
+				updateCanvasHandler(
+					vars["shape"],
+					vars["color"],
+					vars["wheelsLocation"],
+					vars["show3dPreview"]
+				);
+
+				return false;
+			}
+		};
 	};
-};
+})(window.Raphael);
 
 // Surfaceize!
 $(document).ready(function(){
-
 	if(Modernizr.webgl) {
 		// Go!
-		BOTPRINT.SketchaBot.play();
+		Raphael.sketchabot().play();
 	}
 });
