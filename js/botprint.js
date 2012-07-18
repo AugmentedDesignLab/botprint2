2cd71af91230bca24d78d45bca694e2c73894010
Zhongpeng Lin (林中鹏) <lin.zhp@gmail.com>
2012-07-18 12:47:30 -0700

    Create a separate global bindable object called

--- js/botprint.js
+++ js/botprint.js
@@ -3,18 +3,17 @@
  * @author hsanchez@cs.ucsc.edu (Huascar A. Sanchez)
  */
 function Botprint() {
-	var eventBus	= EventBus();
+	var radio	= Bindable();
+	var canvas  	= Canvas2D({elemID: 'canvas2d', app: radio});
+	var preview 	= Preview3D({elemID:'preview3d', app: radio});
+	var sidePanel	= SidePanel({elemClass: 'palette-set a', app: radio});
+
 	var self = {
 		VERSION: '0.0.2',
 		play: function() {
 			preview.animate();
 		}
 	};
-
-	Mixable(self).mix(Bindable());	
-	var canvas  	= Canvas2D({elemID: 'canvas2d', app: self});
-	var preview 	= Preview3D({elemID:'preview3d', app: self});
-	var sidePanel	= SidePanel({elemClass: 'palette-set a', app: self});
 	
 	return self;
 }
