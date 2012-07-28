2f90e4e01093a89026cf5f465e455a10a347bf56
Zhongpeng Lin (林中鹏) <lin.zhp@gmail.com>
2012-07-28 11:15:12 -0700

    fixed a firefox compatibility issue

--- js/botprint/spi/RelativeCoordEvent.js
+++ js/botprint/spi/RelativeCoordEvent.js
@@ -4,7 +4,7 @@
  * @author Zhongpeng Lin
  */
 function RelativeCoordEvent(event) {
-	event.relativeX = event.offsetX || event.layerX;
-	event.relativeY = event.offsetY || event.layerY;
+	event.relativeX = event.offsetX || event.pageX - $(event.currentTarget).offset().left;;
+	event.relativeY = event.offsetY || event.pageY - $(event.currentTarget).offset().top;
 	return event;	
 }
