b17a037deaeb1c214013704ce86c99121eef6608
Zhongpeng Lin (林中鹏) <lin.zhp@gmail.com>
2012-07-30 23:44:28 -0700

    changes based on pull request comments

--- js/botprint/model/Chassis.js
+++ js/botprint/model/Chassis.js
@@ -18,48 +18,8 @@ function Chassis (opts){
 							   {path: self.path, transform: self.transform});
 		},
 		
-		selfIntersecting: function() {
-			function endPoint(path) {
-				var length = path.length;
-				return new Vector2D(path[length-2], path[length-1]);
-			}
-			
-			function equal(vect1, vect2) {
-				if(vect1.x == vect2.x && vect1.y == vect2.y)
-					return true;
-				else
-					return false;
-			}
-			
-			var intersect;
-			for(var i = 1; i < self.path.length; i++) {
-				var start1 = endPoint(self.path[i-1]);
-				var end1 = new Vector2D(self.path[i][5], self.path[i][6]);
-				for(var j = i+1; j < self.path.length; j++) {
-					var start2 = endPoint(self.path[j-1]);
-					if(self.path[i][0] == 'C' && self.path[j][0] == 'C'){
-						var end2 = new Vector2D(self.path[j][5], self.path[j][6]);
-						intersect = Intersection.intersectBezier3Bezier3(
-							start1,
-							new Vector2D(self.path[i][1], self.path[i][2]),
-							new Vector2D(self.path[i][3], self.path[i][4]),
-							end1,
-							start2,
-							new Vector2D(self.path[j][1], self.path[j][2]),
-							new Vector2D(self.path[j][3], self.path[j][4]),
-							end2
-						);
-					} // other line intersections can be added there when needed
-					var crossPonts = intersect.points.select(function(p){
-						// return true only if p is not any of the vertices
-						return !(equal(p, start1) || equal(p, end1) || equal(p, start2) || equal(p, end2));
-					});
-					if(crossPonts.length > 0) {
-						return true;
-					}
-				}
-			}
-			return false;
+		isSelfIntersecting: function() {
+			return IntersectionDetection.isSelfIntersecting(self.path);
 		}
 	};
 
--- spec/ChassisSpec.js
+++ spec/ChassisSpec.js
@@ -2,7 +2,7 @@ describe('A chassis', function() {
 	it('should detect self intersecting', function() {
 		var path = [['M',236,311],['C', 220,339,312,459,347,454],['C', 381,449,459,304,441,281],['C',422,257,251,282,236,311], ['Z']];
 		var chassis = Chassis({path: path});
-		expect(chassis.selfIntersecting()).toBe(false);
+		expect(chassis.isSelfIntersecting()).toBe(false);
 		path = [
 			['M', 286, 220],
 			['C', 289, 189, 517, 195, 515, 227],
@@ -12,6 +12,6 @@ describe('A chassis', function() {
 			['Z']
 		];
 		chassis.path = path;
-		expect(chassis.selfIntersecting()).toBe(true);
+		expect(chassis.isSelfIntersecting()).toBe(true);
 	});
 });
