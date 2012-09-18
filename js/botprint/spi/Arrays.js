5d5feb700f5661a399a7e025e0e939b490798c02
Huascar A. Sanchez <hsanchez@soe.ucsc.edu>
2012-09-18 15:45:56 -0700

    fixed bug in Array util.

--- js/botprint/spi/Arrays.js
+++ js/botprint/spi/Arrays.js
@@ -3,7 +3,7 @@
  *  @author hsanchez@cs.ucsc.edu (Huascar A. Sanchez)
  */
 // Array Remove - By John Resig (MIT Licensed)
-Array.prototype.remove = function(array, from, to) {
+Array.remove = function(array, from, to) {
 	var rest = array.slice((to || from) + 1 || array.length);
 	array.length = from < 0 ? array.length + from : from;
 	return array.push.apply(array, rest);
@@ -11,7 +11,7 @@ Array.prototype.remove = function(array, from, to) {
 
 Array.prototype.select = Array.prototype.filter;
 
-Array.prototype.chunkIt = function(array, num){
+Array.chunkIt = function(array, num){
 	var len = array.length, out = [], i = 0;
 	while (i < len) {
 		var size = Math.ceil((len - i) / num--);
