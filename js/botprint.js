aebe213bd6211d55019951196de8349d830af435
Zhongpeng Lin (林中鹏) <lin.zhp@gmail.com>
2012-10-10 16:46:19 -0700

    add shapes to splashcreen programmatically

--- js/botprint.js
+++ js/botprint.js
@@ -7,12 +7,13 @@ function Botprint() {
 	var canvas  	= Canvas2D({elemID: 'canvas2d', app: radio});
 	var preview 	= Preview3D({elemID:'preview3d', app: radio});
 	var sidePanel	= SidePanel({elemID: 'menu', elemClass: 'palette-set a', app: radio});
+	var splashScreen = SplashScreen({elemID: 'splash'});
 
 	var self = {
 		VERSION: '0.0.2',
 		play: function() {
 			preview.animate();
-			$('#splash').modal();
+			splashScreen.show();
 		}
 	};
 	
