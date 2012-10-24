9ec9cf83c169de3ed7ec5fd85ef2d46efb140372
Zhongpeng Lin (林中鹏) <zlin5@ucsc.edu>
2012-10-23 21:33:32 -0700

    add more inner shape punch hole strategy

--- js/botprint/model/Chassis.js
+++ js/botprint/model/Chassis.js
@@ -23,7 +23,7 @@ function Chassis (opts){
 		
 		// assuming the chassis is a Catmull-rom curve
 		get punchHoles() {
-			return InwardNormalStrategy.getPunchHoles(self.path);
+			return InnerShapeStrategy.getPunchHoles(self.path);
 		},
 
 		accept: function(visitor) {
